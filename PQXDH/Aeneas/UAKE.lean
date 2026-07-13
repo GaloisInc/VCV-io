92832f0229954f424b3542d59aa59cecdd6bc4f7
Ben Hamlin <hamlinb@galois.com>
2026-07-13 14:29:31 -0700

    Close 10 of 13 sorries on reduction of Aeneas-extracted PQXDH to spec

--- PQXDH/Aeneas/UAKE.lean
+++ PQXDH/Aeneas/UAKE.lean
@@ -940,6 +940,14 @@ def mapStepResult {St₁ St₂ W : Type} (σ : St₁ → St₂) :
   | .complete st => .complete (σ st)
   | .reject => .reject
 
+private lemma mapInitResult_state {St₁ St₂ W : Type} (σ : St₁ → St₂)
+    (r : Party.InitResult St₁ W) : (mapInitResult σ r).state = σ r.state := by
+  cases r <;> rfl
+
+private lemma mapInitResult_opening {St₁ St₂ W : Type} (σ : St₁ → St₂)
+    (r : Party.InitResult St₁ W) : (mapInitResult σ r).opening = r.opening := by
+  cases r <;> rfl
+
 section GroupModelBridge
 
 variable [Field F] [SampleableType F] [AddCommGroup ECKey] [Module F ECKey]
@@ -948,18 +956,146 @@ variable [Field F] [SampleableType F] [AddCommGroup ECKey] [Module F ECKey]
 private lemma genOPK_toSpec (hM : ECGroupModel P gen privEnc) (hasOPK : Bool) :
     genOPK P.ecKeygen hasOPK
       = Option.map (kpOfPair privEnc) <$> _root_.PQXDH.genOPK (F := F) gen hasOPK := by
-  sorry
+  rw [hM.keygen_eq]
+  cases hasOPK <;> simp [genOPK, _root_.PQXDH.genOPK, Functor.map_map]
 
 private lemma setup_toSpec (hM : ECGroupModel P gen privEnc) (msg : Msg) :
     setup P msg
       = Prod.map (ukOfSpec privEnc) (tkOfSpec privEnc) <$>
           _root_.PQXDH.setup (specParams P F gen) msg := by
-  sorry
+  simp only [setup, _root_.PQXDH.setup, hM.keygen_eq, map_bind, bind_map_left]
+  rfl
 
+omit [Field F] [SampleableType F] [AddCommGroup ECKey] [Module F ECKey] in
 private lemma publish_toSpec
     (rp : _root_.PQXDH.RecipientParameters F ECKey PQPK PQSK SPK SSK S) :
     publish P (rpOfSpec privEnc rp) = _root_.PQXDH.publish (specParams P F gen) rp := by
-  sorry
+  refine bind_congr fun σ => ?_
+  rcases hopk : rp.opkB with _ | opk <;>
+    simp [rpOfSpec, kpOfPair, hopk, specParams]
+
+omit [Field F] [SampleableType F] [AddCommGroup ECKey] [Module F ECKey] in
+private lemma kpOfPair_public (p : ECKey × F) :
+    (kpOfPair privEnc p).public_key = p.1 :=
+  rfl
+
+private lemma pqxdh_initiate_groupModel (hM : ECGroupModel P gen privEnc)
+    (p₁ p₂ : ECKey × F) (ikB spk : ECKey) (opk : Option ECKey) (pqpk : PQPK)
+    (coins : Coins) {ss : SS} {ct : CT} {ks : Key × Key × Key}
+    (henc : pqxdh.mlkem_encapsulate pqpk coins = .ok (ss, ct))
+    (hks : deriveKeys (_root_.PQXDH.DH p₁.2 spk) (_root_.PQXDH.DH p₂.2 ikB)
+      (_root_.PQXDH.DH p₂.2 spk) (Option.map (fun o => _root_.PQXDH.DH p₂.2 o) opk) ss
+      = .ok ks) :
+    pqxdh.pqxdh_initiate
+      { our_identity_key_pair := kpOfPair privEnc p₁
+        our_ephemeral_key_pair := kpOfPair privEnc p₂
+        their_identity_key := ikB
+        their_signed_pre_key := spk
+        their_one_time_pre_key := opk
+        their_kyber_pre_key := pqpk } coins
+      = .ok ⟨⟨ks.1, ks.2.1, ks.2.2⟩, ct⟩ := by
+  unfold pqxdh.pqxdh_initiate
+  cases opk with
+  | none =>
+      simp only [Option.map_none] at hks
+      cases hsi : pqxdh.pqxdh_secret_input (_root_.PQXDH.DH p₁.2 spk)
+          (_root_.PQXDH.DH p₂.2 ikB) (_root_.PQXDH.DH p₂.2 spk) ss with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | ok si =>
+      cases hokm : pqxdh.hkdf_sha256_derive si.to_slice pqxdh.PQXDH_LABEL.to_slice with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | ok okm =>
+      cases hsplit : pqxdh.derive_split okm with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | ok keys =>
+      obtain ⟨rk, ck, pk⟩ := keys
+      simp only [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit, Aeneas.Std.bind_tc_ok,
+        Aeneas.Std.Result.ok.injEq] at hks
+      subst hks
+      simp [kpOfPair, hM.agree_eq, Aeneas.Std.lift, henc, hsi, hokm, hsplit]
+  | some o =>
+      simp only [Option.map_some] at hks
+      cases hsi : pqxdh.pqxdh_secret_input_with_opk (_root_.PQXDH.DH p₁.2 spk)
+          (_root_.PQXDH.DH p₂.2 ikB) (_root_.PQXDH.DH p₂.2 spk)
+          (_root_.PQXDH.DH p₂.2 o) ss with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | ok si =>
+      cases hokm : pqxdh.hkdf_sha256_derive si.to_slice pqxdh.PQXDH_LABEL.to_slice with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | ok okm =>
+      cases hsplit : pqxdh.derive_split okm with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | ok keys =>
+      obtain ⟨rk, ck, pk⟩ := keys
+      simp only [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit, Aeneas.Std.bind_tc_ok,
+        Aeneas.Std.Result.ok.injEq] at hks
+      subst hks
+      simp [kpOfPair, hM.agree_eq, Aeneas.Std.lift, henc, hsi, hokm, hsplit]
+
+private lemma pqxdh_accept_groupModel (hM : ECGroupModel P gen privEnc)
+    (q₁ q₂ : ECKey × F) (opkq : Option (ECKey × F)) (pqsk : PQSK)
+    (ikA ekA : ECKey) (ct : CT) {ss : SS} {ks : Key × Key × Key}
+    (hdec : pqxdh.mlkem_decapsulate pqsk ct = .ok ss)
+    (hks : deriveKeys (_root_.PQXDH.DH q₂.2 ikA) (_root_.PQXDH.DH q₁.2 ekA)
+      (_root_.PQXDH.DH q₂.2 ekA) (Option.map (fun o => _root_.PQXDH.DH o.2 ekA) opkq) ss
+      = .ok ks) :
+    pqxdh.pqxdh_accept
+      { our_identity_key_pair := kpOfPair privEnc q₁
+        our_signed_pre_key_pair := kpOfPair privEnc q₂
+        our_one_time_pre_key_pair := opkq.map (kpOfPair privEnc)
+        our_kyber_secret_key := pqsk
+        their_identity_key := ikA
+        their_ephemeral_key := ekA
+        their_kyber_ciphertext := ct } = .ok (some ⟨ks.1, ks.2.1, ks.2.2⟩) := by
+  unfold pqxdh.pqxdh_accept
+  cases opkq with
+  | none =>
+      simp only [Option.map_none] at hks ⊢
+      cases hsi : pqxdh.pqxdh_secret_input (_root_.PQXDH.DH q₂.2 ikA)
+          (_root_.PQXDH.DH q₁.2 ekA) (_root_.PQXDH.DH q₂.2 ekA) ss with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | ok si =>
+      cases hokm : pqxdh.hkdf_sha256_derive si.to_slice pqxdh.PQXDH_LABEL.to_slice with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | ok okm =>
+      cases hsplit : pqxdh.derive_split okm with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | ok keys =>
+      obtain ⟨rk, ck, pk⟩ := keys
+      simp only [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit, Aeneas.Std.bind_tc_ok,
+        Aeneas.Std.Result.ok.injEq] at hks
+      subst hks
+      simp [kpOfPair, hM.canonical_eq, hM.agree_eq, Aeneas.Std.lift, hdec, hsi, hokm, hsplit]
+  | some o =>
+      simp only [Option.map_some] at hks ⊢
+      cases hsi : pqxdh.pqxdh_secret_input_with_opk (_root_.PQXDH.DH q₂.2 ikA)
+          (_root_.PQXDH.DH q₁.2 ekA) (_root_.PQXDH.DH q₂.2 ekA)
+          (_root_.PQXDH.DH o.2 ekA) ss with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi] at hks
+      | ok si =>
+      cases hokm : pqxdh.hkdf_sha256_derive si.to_slice pqxdh.PQXDH_LABEL.to_slice with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm] at hks
+      | ok okm =>
+      cases hsplit : pqxdh.derive_split okm with
+      | fail e => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | div => simp [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit] at hks
+      | ok keys =>
+      obtain ⟨rk, ck, pk⟩ := keys
+      simp only [deriveKeys, Aeneas.Std.lift, hsi, hokm, hsplit, Aeneas.Std.bind_tc_ok,
+        Aeneas.Std.Result.ok.injEq] at hks
+      subst hks
+      simp [kpOfPair, hM.canonical_eq, hM.agree_eq, Aeneas.Std.lift, hdec, hsi, hokm, hsplit]
 
 private lemma initiate_toSpec (hM : ECGroupModel P gen privEnc)
     (hencTotal : EncapsTotalAll) (hkdfTotal : DeriveKeysTotal)
@@ -967,7 +1103,36 @@ private lemma initiate_toSpec (hM : ECGroupModel P gen privEnc)
     (bundle : PreKeyBundle ECKey PQPK S IdC IdK) :
     initiate P (ukOfSpec privEnc uk) bundle
       = _root_.PQXDH.initiate (specParams P F gen) uk bundle := by
-  sorry
+  simp only [initiate, _root_.PQXDH.initiate, ukOfSpec, specParams, hM.keygen_eq,
+    bind_map_left]
+  by_cases hpin : bundle.ikB = uk.ikB
+  · simp only [hpin, ne_eq, not_true_eq_false, if_false]
+    refine bind_congr fun _ => ?_
+    refine bind_congr fun okSPK => ?_
+    refine bind_congr fun okPQPK => ?_
+    cases okSPK with
+    | false => simp
+    | true =>
+    cases okPQPK with
+    | false => simp
+    | true =>
+    simp only [Bool.and_self, Bool.not_true, Bool.false_eq_true, if_false]
+    refine bind_congr fun _ => ?_
+    refine bind_congr fun a => ?_
+    simp only [pqkem, bind_assoc]
+    refine bind_congr fun coins => ?_
+    obtain ⟨⟨ss, ct⟩, henc⟩ := hencTotal bundle.pqpkB.1 coins
+    obtain ⟨ks, hks⟩ := hkdfTotal (_root_.PQXDH.DH uk.ikA.2 bundle.spkB.1)
+      (_root_.PQXDH.DH a.2 uk.ikB) (_root_.PQXDH.DH a.2 bundle.spkB.1)
+      (Option.map (fun o => _root_.PQXDH.DH a.2 o) (bundle.opkB.map Prod.fst)) ss
+    rw [pqxdh_initiate_groupModel P gen privEnc hM uk.ikA a uk.ikB bundle.spkB.1
+      (bundle.opkB.map Prod.fst) bundle.pqpkB.1 coins henc hks]
+    have hmap : Option.map (fun o => _root_.PQXDH.DH a.2 o) (bundle.opkB.map Prod.fst)
+        = bundle.opkB.map fun opk => _root_.PQXDH.DH a.2 opk.1 := by
+      cases bundle.opkB <;> rfl
+    rw [hmap] at hks
+    simp [henc, hks, getOk, kpOfPair]
+  · simp [hpin]
 
 private lemma accept_toSpec [DecidableEq IdC] [DecidableEq IdK]
     (hM : ECGroupModel P gen privEnc)
@@ -975,7 +1140,46 @@ private lemma accept_toSpec [DecidableEq IdC] [DecidableEq IdK]
     (rp : _root_.PQXDH.RecipientParameters F ECKey PQPK PQSK SPK SSK S)
     (im : InitialMessage ECKey CT C IdC IdK) :
     accept P (rpOfSpec privEnc rp) im = _root_.PQXDH.accept (specParams P F gen) rp im := by
-  sorry
+  simp only [accept, _root_.PQXDH.accept, rpOfSpec, specParams, kpOfPair_public,
+    Option.map_map, Function.comp_def]
+  by_cases hguard : im.idSPK ≠ P.idEC rp.spkB.1 ∨ im.idPQPK ≠ P.idKEM rp.pqpkB.1 ∨
+      im.idOPK ≠ Option.map (fun opk => P.idEC opk.1) rp.opkB
+  · simp [hguard]
+  · simp only [hguard, if_false]
+    cases hdec : pqxdh.mlkem_decapsulate rp.pqpkB.2 im.ct with
+    | ok ss =>
+        obtain ⟨ks, hks⟩ := hkdfTotal (_root_.PQXDH.DH rp.spkB.2 im.ikA)
+          (_root_.PQXDH.DH rp.ikB.2 im.ekA) (_root_.PQXDH.DH rp.spkB.2 im.ekA)
+          (Option.map (fun opk => _root_.PQXDH.DH opk.2 im.ekA) rp.opkB) ss
+        rw [pqxdh_accept_groupModel P gen privEnc hM rp.ikB rp.spkB rp.opkB rp.pqpkB.2
+          im.ikA im.ekA im.ct hdec hks]
+        simp only [pqkem, hdec, pure_bind]
+        simp only [hks, getOk]
+        cases P.aead.decrypt ks.2.1 (im.ikA, rp.ikB.1, rp.pqpkB.1) im.ctxt <;> rfl
+    | fail e =>
+        have hacc : pqxdh.pqxdh_accept
+            { our_identity_key_pair := kpOfPair privEnc rp.ikB
+              our_signed_pre_key_pair := kpOfPair privEnc rp.spkB
+              our_one_time_pre_key_pair := rp.opkB.map (kpOfPair privEnc)
+              our_kyber_secret_key := rp.pqpkB.2
+              their_identity_key := im.ikA
+              their_ephemeral_key := im.ekA
+              their_kyber_ciphertext := im.ct } = .fail e := by
+          simp [pqxdh.pqxdh_accept, kpOfPair, hM.canonical_eq, hM.agree_eq, hdec]
+        rw [hacc]
+        simp [pqkem, hdec]
+    | div =>
+        have hacc : pqxdh.pqxdh_accept
+            { our_identity_key_pair := kpOfPair privEnc rp.ikB
+              our_signed_pre_key_pair := kpOfPair privEnc rp.spkB
+              our_one_time_pre_key_pair := rp.opkB.map (kpOfPair privEnc)
+              our_kyber_secret_key := rp.pqpkB.2
+              their_identity_key := im.ikA
+              their_ephemeral_key := im.ekA
+              their_kyber_ciphertext := im.ct } = .div := by
+          simp [pqxdh.pqxdh_accept, kpOfPair, hM.canonical_eq, hM.agree_eq, hdec]
+        rw [hacc]
+        simp [pqkem, hdec]
 
 omit [Field F] [SampleableType F] [AddCommGroup ECKey] [Module F ECKey] in
 private lemma confirm_toSpec [DecidableEq Msg]
@@ -999,7 +1203,23 @@ private lemma initiator_step_toSpec [DecidableEq Msg]
     (initiator P).step (Sum.map (ukOfSpec privEnc) id st) w
       = mapStepResult (Sum.map (ukOfSpec privEnc) id) <$>
           (_root_.PQXDH.initiator (specParams P F gen)).step st w := by
-  sorry
+  rcases st with p | ctx | k
+  · cases w with
+    | bundle b =>
+        simp only [initiator, _root_.PQXDH.initiator, Sum.map_inl,
+          initiate_toSpec P gen privEnc hM hencTotal hkdfTotal p b, map_bind]
+        refine bind_congr fun r => ?_
+        rcases r with _ | ⟨im, ctx⟩ <;> simp [mapStepResult]
+    | initial im => simp [initiator, _root_.PQXDH.initiator, mapStepResult]
+    | confirmation c => simp [initiator, _root_.PQXDH.initiator, mapStepResult]
+  · cases w with
+    | bundle b => simp [initiator, _root_.PQXDH.initiator, mapStepResult]
+    | initial im => simp [initiator, _root_.PQXDH.initiator, mapStepResult]
+    | confirmation conf =>
+        simp only [initiator, _root_.PQXDH.initiator, Sum.map_inr, id_eq]
+        rw [confirm_toSpec (F := F) P gen ctx conf]
+        cases _root_.PQXDH.confirm (specParams P F gen) ctx conf <;> simp [mapStepResult]
+  · cases w <;> simp [initiator, _root_.PQXDH.initiator, mapStepResult]
 
 private lemma initiator_output_toSpec [DecidableEq Msg]
     (st : _root_.PQXDH.InitiatorParameters F ECKey SPK Msg ⊕
@@ -1014,7 +1234,17 @@ private lemma recipient_init_toSpec [DecidableEq IdC] [DecidableEq IdK]
     (recipient P hasOPK).init (tkOfSpec privEnc tk)
       = mapInitResult (Sum.map (rpOfSpec privEnc) id) <$>
           (_root_.PQXDH.recipient (specParams P F gen) hasOPK).init tk := by
-  sorry
+  simp only [recipient, _root_.PQXDH.recipient, genOPK_toSpec P gen privEnc hM hasOPK,
+    specParams, pqkem, tkOfSpec, map_bind, bind_map_left]
+  refine bind_congr fun opkB => ?_
+  refine bind_congr fun pqpkB => ?_
+  have hrp : (⟨kpOfPair privEnc tk.ikB, tk.sigkB, kpOfPair privEnc tk.spkB,
+      tk.spkSigB, Option.map (kpOfPair privEnc) opkB, pqpkB⟩ :
+        RecipientParameters SPK SSK S)
+      = rpOfSpec privEnc ⟨tk.ikB, tk.sigkB, tk.spkB, tk.spkSigB, opkB, pqpkB⟩ := rfl
+  rw [hrp, publish_toSpec]
+  refine bind_congr fun bundle => ?_
+  simp [mapInitResult, rpOfSpec]
 
 private lemma recipient_step_toSpec [DecidableEq IdC] [DecidableEq IdK]
     (hM : ECGroupModel P gen privEnc)
@@ -1024,7 +1254,16 @@ private lemma recipient_step_toSpec [DecidableEq IdC] [DecidableEq IdK]
     (recipient P hasOPK).step (Sum.map (rpOfSpec privEnc) id st) w
       = mapStepResult (Sum.map (rpOfSpec privEnc) id) <$>
           (_root_.PQXDH.recipient (specParams P F gen) hasOPK).step st w := by
-  sorry
+  rcases st with rp | k
+  · cases w with
+    | bundle b => simp [recipient, _root_.PQXDH.recipient, mapStepResult]
+    | initial im =>
+        simp only [recipient, _root_.PQXDH.recipient, Sum.map_inl,
+          accept_toSpec P gen privEnc hM hkdfTotal rp im, map_bind]
+        refine bind_congr fun r => ?_
+        rcases r with _ | ctx <;> simp [mapStepResult, specParams]
+    | confirmation c => simp [recipient, _root_.PQXDH.recipient, mapStepResult]
+  · cases w <;> simp [recipient, _root_.PQXDH.recipient, mapStepResult]
 
 private lemma recipient_output_toSpec [DecidableEq IdC] [DecidableEq IdK] (hasOPK : Bool)
     (st : _root_.PQXDH.RecipientParameters F ECKey PQPK PQSK SPK SSK S ⊕ Key) :
@@ -1125,7 +1364,40 @@ private lemma kdfPRFDH_advantage_toSpec {P : Parameters SPK SSK S C Msg IdC IdK}
     (D : PRFScheme.PRFAdversary (ECKey × ECKey × Option ECKey × SS) (Key × Key × Key)) :
     (_root_.PQXDH.kdfPRFDH (specParams P F gen)).prfAdvantage D
       = (kdfPRFDH P).prfAdvantage D := by
-  sorry
+  have hreal : (_root_.PQXDH.kdfPRFDH (specParams P F gen)).prfRealExp D
+      = (kdfPRFDH P).prfRealExp D := by
+    unfold PRFScheme.prfRealExp
+    rw [show (kdfPRFDH P).keygen = P.ecKeygen from rfl, hM.keygen_eq,
+      show _root_.PQXDH.dhKeygen (F := F) gen
+        = (do let a ← $ᵗ F; pure (a • gen, a)) from rfl]
+    simp only [bind_map_left, bind_assoc, pure_bind]
+    exact bind_congr fun c => rfl
+  unfold PRFScheme.prfAdvantage
+  rw [hreal]
+
+private lemma nominalDDHExpReal_toSpec (hM : ECGroupModel P gen privEnc)
+    (D : DiffieHellman.NominalDDHAdversary ECKey) :
+    DiffieHellman.nominalDDHExpReal P.ecKeygen pqxdh.KeyPair.public_key x25519DH D
+      = DiffieHellman.nominalDDHExpReal (DiffieHellman.groupKeygen (F := F) gen)
+          Prod.fst (fun kp pk => kp.2 • pk) D := by
+  unfold DiffieHellman.nominalDDHExpReal
+  rw [hM.keygen_eq, show _root_.PQXDH.dhKeygen (F := F) gen
+    = DiffieHellman.groupKeygen (F := F) gen from rfl]
+  simp only [bind_map_left]
+  refine bind_congr fun kpA => ?_
+  refine bind_congr fun kpB => ?_
+  simp [x25519DH, kpOfPair, hM.agree_eq, getOk, _root_.PQXDH.DH]
+
+private lemma nominalDDHExpRand_toSpec (hM : ECGroupModel P gen privEnc)
+    (D : DiffieHellman.NominalDDHAdversary ECKey) :
+    DiffieHellman.nominalDDHExpRand P.ecKeygen pqxdh.KeyPair.public_key D
+      = DiffieHellman.nominalDDHExpRand (DiffieHellman.groupKeygen (F := F) gen)
+          Prod.fst D := by
+  unfold DiffieHellman.nominalDDHExpRand
+  rw [hM.keygen_eq, show _root_.PQXDH.dhKeygen (F := F) gen
+    = DiffieHellman.groupKeygen (F := F) gen from rfl]
+  simp only [bind_map_left]
+  exact bind_congr fun kpA => bind_congr fun kpB => bind_congr fun kpC => rfl
 
 private lemma ddh_advantage_toSpec {P : Parameters SPK SSK S C Msg IdC IdK}
     (hM : ECGroupModel P gen privEnc)
@@ -1133,7 +1405,10 @@ private lemma ddh_advantage_toSpec {P : Parameters SPK SSK S C Msg IdC IdK}
     _root_.DiffieHellman.ddhDistAdvantage gen D
       = DiffieHellman.nominalDDHDistAdvantage P.ecKeygen pqxdh.KeyPair.public_key
           x25519DH (D gen) := by
-  sorry
+  rw [DiffieHellman.ddhDistAdvantage_eq_nominalDDHDistAdvantage]
+  unfold DiffieHellman.nominalDDHDistAdvantage
+  rw [nominalDDHExpReal_toSpec P gen privEnc hM (D gen),
+    nominalDDHExpRand_toSpec P gen privEnc hM (D gen)]
 
 end GroupModelBridge
 
